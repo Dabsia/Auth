e6103ed9ca8b4adb932b0d6c0cebc9a98e3f9c7b
Dabsia <dabojohnson@gmail.com>
2022-04-30 22:21:50 -0700

    Made functionality for changing the password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,18 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import AuthContext from '../../store/auth-context';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
 
   const API_KEY = 'AIzaSyDJBfsvccXwiw7-B544jduXH5TRzRoEsHs'
+  const Navigate = useNavigate()
+
+  
+
+  const authCtx = useContext(AuthContext)
+
 
   const emailInputRef = useRef()
   const passwordInputRef = useRef()
@@ -24,38 +32,45 @@ const AuthForm = () => {
 
     // Optional: Add Validation
     setIsLoading(true)
+ 
+    let url;
     if (isLogin) {
-
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`
     }
     else{
-      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(res => {
-        setIsLoading(false)
-        if (res.ok) {
-          //...
-          console.log('Your account has been created succesfully')
-        }
-        else{
-          res.json().then(data => {
-            let errorMessage = 'Authentication failed';
-            if (data && data.error.message) {
-              errorMessage = data.error.message;
-            }
-              alert(errorMessage)
-          });
-        }
-      })
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
     }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      setIsLoading(false) // changes the state while sending the request
+      if (res.ok) {
+        
+        return res.json()
+      }
+      else{
+        res.json().then(data => {
+          let errorMessage = 'Authentication failed';
+          if (data && data.error.message) {
+            errorMessage = data.error.message;
+          }
+            alert(errorMessage)
+            throw new Error(errorMessage)
+        });
+      }
+    }).then(data => {
+      authCtx.login(data.idToken)
+      Navigate('/profile')
+    }).catch(err => alert(err.message))
 
   }
 
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,60 @@
+import { useRef, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+
+  const newPasswordInputRef = useRef()
+
+  const Navigate = useNavigate()
+
+  const API_KEY = 'AIzaSyDJBfsvccXwiw7-B544jduXH5TRzRoEsHs'
+
+  const authCtx = useContext(AuthContext)
+
+  const submitHandler = (e) =>{
+    e.preventDefault()
+
+    const enteredNewPassword = newPasswordInputRef.current.value
+    alert(enteredNewPassword)
+
+    // Add validation
+
+    // This function changes password of the user from the profile
+    
+    if(enteredNewPassword.length === 0){
+      return 
+    }
+    else{
+      
+      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`, {
+        method: 'POST',
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken : false
+        }),
+        headers: {
+          'Content-Type' : 'application/json'
+          
+        }
+      }).then(res => {
+        //Assumes Always succeed;
+  
+        alert('success')
+        Navigate('/')
+      })
+    }
+
+
+  }
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' minLength='7' ref = {newPasswordInputRef} id='new-password' />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
